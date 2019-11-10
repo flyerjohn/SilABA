e99b134da114c100ef545c7b825cb5efce1ef1bd
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2019-11-10 15:32:01 -0300

    Adicionando a funcao comparaCartas

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-void escolherModo(int check){
+int comparaCartas(char carta1[6], char carta2[6]){ // funcao que se encontrar divergencias nas palavras, retorna 0
+    for(int i = 0; i < 6; i++){
+        if(carta1[i] != carta2[i]){
+            printf("%c x %c \n", carta1[i], carta2[i]);
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void escolherModo(int check){ // funcao para escolher o modo de jogo
     char comp[6] = "bola", pal[6];
     int numC = 4, numP = 0, boolCmp;
     switch (check)
@@ -35,9 +45,13 @@ void escolherModo(int check){
 
 int main()
 {
-    int check;
+    char * c1 = "Bola";
+    char * c2 = "Bol"; 
+    int check, m1;
     printf("Escolha o modo de jogo, 1 ou 2: ");
     scanf("%d", &check);
     escolherModo(check);
+    m1 = comparaCartas(c1, c2);
+    printf("%d", m1); // checagem do retorno da funcao comparaCartas
     return 0;
 }
